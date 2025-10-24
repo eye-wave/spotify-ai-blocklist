eeb88ceba666422d515a1f699c8134da3c16ef3e
_eyewave <89079979+eye-wave@users.noreply.github.com>
2025-10-24 14:51:48 +0200

    remove code distractions

---
+++ main.js
@@ -0,0 +1,82 @@
+/**
+ * @typedef {Object} Trashbin
+ * @property {Record<string, boolean>} artists
+ * @property {Record<string, boolean>} songs
+ */
+
+import fs from "node:fs";
+import readline from "node:readline";
+
+main();
+async function main() {
+  const filePath = new URL("./ai-list.txt", import.meta.url).pathname;
+  const trashPath = new URL("./ai-trashbin.txt", import.meta.url).pathname;
+
+  const rl = readline.createInterface({ input: fs.createReadStream(filePath) });
+
+  /** @type {Set<string>} */
+  const seen = new Set();
+
+  /** @type {Trashbin} */
+  const trash = {
+    artists: {},
+    songs: {},
+  };
+
+  let duplicates = 0;
+
+  for await (const line of rl) {
+    const id = parseId(line);
+
+    if (!id) continue;
+
+    const url = "https://open.spotify.com/artist/" + id;
+    const trashId = "spotify:artist:" + id;
+
+    if (seen.has(url)) duplicates++;
+    else {
+      seen.add(url);
+      trash.artists[trashId] = true;
+    }
+  }
+
+  console.log(
+    duplicates === 0
+      ? "No duplicates found."
+      : `Found ${duplicates} duplicate entries.`
+  );
+
+  await writeFile([...seen], filePath);
+  await writeFile(trash, trashPath);
+}
+
+/**
+ * @param {string} line
+ */
+function parseId(line) {
+  const index = line.lastIndexOf("/");
+  if (index === -1 || index + 23 > line.length) return null;
+
+  for (let i = 0; i < 22; i++) {
+    const char = line.charCodeAt(index + i + 1);
+
+    if (char < 48) return null;
+    if (char > 57 && char < 65) return null;
+    if (char > 90 && char < 97) return null;
+    if (char > 122) return null;
+  }
+
+  return line.slice(index + 1, index + 23);
+}
+
+/**
+ * @param {string} file
+ * @param {URL} path
+ */
+async function writeFile(file, path) {
+  const content = Array.isArray(file)
+    ? file.join("\n")
+    : JSON.stringify(file, null, 2);
+
+  await fs.promises.writeFile(path, content).catch(console.error);
+}
